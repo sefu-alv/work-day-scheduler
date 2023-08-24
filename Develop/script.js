755c56b985add1c19c5cd4c03d06b6a9bcfef070
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-24 15:04:27 -0700

    got the colors to change according to the time, accomplished this by changing the id to 13,14,15... instead of 1,2,3,4,5

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,7 @@
-// The global variables
-var today = dayjs();
-
-// This function saves the user input into the local storage
 $(document).ready(function () {
+  var today = dayjs();
+  var date = today.date();
+  var suffix = getSuffix(date);
   $(document).on("click", ".saveBtn", function () {
     var input = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
@@ -11,31 +10,47 @@ $(document).ready(function () {
   });
   // This function checks for all id's that have "hour" and compares the value to
   // current time. If value matches criteria then the classes are changed
-  $(document).ready(function () {
+  function time() {
     var currentTime = dayjs().hour();
-    $('[id^="hour-"]').each(function () {
-      var idEl = $(this).attr("id");
-      var idSplit = idEl.split("-");
-      var hour = idSplit[1];
-      if (hour === currentTime) {
+  
+    $('.time-block').each(function () {
+      var hour = $(this).attr("id").split("-")[1];
+      console.log(hour);
+      console.log(currentTime);
+      if (hour < currentTime) {
+        console.log("Setting past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (hour == currentTime) {
+        console.log("Setting present");
         $(this).removeClass("past");
         $(this).removeClass("future");
         $(this).addClass("present");
-      } else if (hour < currentTime) {
-        $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-      } else if (hour > currentTime) {
+      } else { (hour  > currentTime) 
+        console.log("Setting future");
         $(this).addClass("future");
         $(this).removeClass("past");
         $(this).removeClass("present");
+      
       }
       // retrieves stored text
-      var storeItem = localStorage.getItem(idEl);
+      var storeItem = localStorage.getItem(hour);
       // finds the class description and adds the value of stored item
       $(this).find(".description").val(storeItem);
     });
-  });
+  }
   //displays the current time
-  $("#currentDay").text(today.format("dddd, MMMM DD "));
+  $("#currentDay").text(today.format("dddd, MMMM") + " " + date + suffix);
+  //displays the dates suffix 
+  function getSuffix(number) {
+    switch(number % 10 ){
+      case 1: return"st";
+      case 2:return "nd";
+      case 3: return"rd";
+      default: return "th";
+    }
+  }
+
+  time();
 });
